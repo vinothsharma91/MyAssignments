2dce0ed20aee5641b26a18c0d64ba373f17f1f90
vinothsharma91 <108619684+vinothsharma91@users.noreply.github.com>
2025-03-02 14:06:49 +0530

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,22 @@
+package Week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float browserVersion = 100.2f;
+		String browserName = "Firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+		System.out.println("The browser version is: "+ browserVersion);
+		System.out.println("The browser Name is: "+ browserName);
+		System.out.println("The browser is visible: "+ isVisible);
+		System.out.println("The browser release year is: "+ releaseYear);
+		System.out.println("The browser Logo is: "+ browserLogo);
+
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,10 @@
+package Week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Welcome to TestLeaf");
+	}
+}
